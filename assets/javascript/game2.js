ac075a699bdf4cf3bf0dec4aa2a79175926ade28
Héctor Morales <hector@morales.is>
2019-06-05 22:27:26 -0400

    working game

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -1,37 +1,42 @@
 // characters
-let char1 = {
+let c1 = {
     'name': 'Sasa',
     'Health Points': 0,
     'Attack Power': 0,
     'Counter Attack Power': 0,
 };
-let char2 = {
+let c2 = {
     'name': 'Pratik',
     'Health Points': 0,
     'Attack Power': 0,
     'Counter Attack Power': 0,
 };
-let char3 = {
+let c3 = {
     'name': 'Nuta',
     'Health Points': 0,
     'Attack Power': 0,
     'Counter Attack Power': 0,
 };
-let char4 = {
+let c4 = {
     'name': 'Plikiv',
     'Health Points': 0,
     'Attack Power': 0,
     'Counter Attack Power': 0,
 };
 
-let minPower = 50;
-let maxPower = 120;
+let minPower = 10;
+let maxPower = 25;
+
+let minHealth = 100;
+let maxHealth = 200;
 
 let playerSelected = enemySelected = false;
 
-let playerSelectedId = enemeySelectedId = 0 ;
+let playerSelectedId = enemeySelectedId = 0;
 
-function attack(){
+let numberOfWins = 0;
+
+function attack() {
     // Attack Power is 6, each attack will increase the Attack Power by 6
 
 }
@@ -45,27 +50,23 @@ function randomNum(min, max) {
 
 // set the random num to each attribute of each character
 function setRandom(char) {
-    char['Health Points']= randomNum(minPower, maxPower);
-    char['Attack Power']= randomNum(minPower, maxPower);
-    char['Counter Attack Power']= randomNum(minPower, maxPower);
+    char['Health Points'] = randomNum(minHealth, maxHealth);
+    char['Attack Power'] = randomNum(8, 8);
+    char['Counter Attack Power'] = randomNum(minPower, maxPower);
 }
 
-function setInfo(charId){
-    let characterObj = eval('char' + charId);
-    
-    setRandom(characterObj);
-
+function setInfo(charId,firstSet = false) {
+    let characterObj = eval('c' + charId);
+    if(firstSet){
+        setRandom(characterObj);
+    }
     let name = characterObj['name'];
-    let aPower = characterObj['Attack Power'];
+    let hPoints = characterObj['Health Points'];
     let nameID = '#c' + charId + 'name';
-    let powerID = '#c' + charId + 'power';
+    let healthId = '#c' + charId + 'health';
     $(nameID).text(name);
-    $(powerID).text('Power: ' + aPower);
-
-
+    $(healthId).text('Health: ' + hPoints);
 }
-
-
 
 // sum of scores
 function sumScore(currentNum, sumNum) {
@@ -79,26 +80,70 @@ function minusPower(currentNum, minusNum) {
     return total;
 }
 
+function fightAction(p1,p2){
+    let p1Power = p1['Attack Power'];
+    p1['Attack Power'] += 8;
+    let p1Health = p1['Health Points'];
+    let p2Power = p2['Counter Attack Power'];
+    let p2Health = p2['Health Points'];
 
-// begin when doc ready
-$(function () {
+    let firstFight = p2Health - p1Power;
+    let secondFight =  p1Health - p2Power;
+
+    p2['Health Points'] = firstFight;
+    p1['Health Points'] = secondFight;
+
     for (let i = 1; i < 5; i++) {
         setInfo(i);
     }
-    let charactersList = $('.chracters');
-    charactersList.click(function(){
+
+    if(secondFight < 0){
+        $('#reset').show();
+        $('#reset').click(function() {
+            location.reload();
+        });
+        $('#fightBtn').prop('disabled', true);
+        return "Game Over";
+    } else if (firstFight < 0){
+        $('#fightBtn').prop('disabled', true);
+        enemySelected = false;
+        numberOfWins++;
+        $('#' + enemeySelectedId).remove();
+        if(numberOfWins !== 3){
+            return "You defeated " + p2['name'] + ", select your next opponent";
+        } else {
+            $('#reset').show();
+            $('#reset').click(function() {
+                location.reload();
+            });
+            return "You won!";
+        }
+    } else {
+        return "You attacked " + p2['name'] + " for " + p1Power + " damage. " + p2['name'] + " attacked back for " + p2Power + " damage.";
+    }
+
+}
+
+// begin when doc ready
+$(function () {
+    $('#reset').hide();
+    for (let i = 1; i < 5; i++) {
+        setInfo(i,true);
+    };
+    $('.chracters').click(function () {
 
         let selectedChar = $(this).parent();
-        if (!playerSelected){
+        if (!playerSelected) {
             $('#instructions').hide();
+            $('#topBanner').hide();
             playerSelectedId = $(selectedChar).attr("id");
             selectedChar.remove();
             playerSelected = true;
             $('#player').append(selectedChar);
             $('#playerCharacter').text("Your character:");
             $('#info').text("Select your enemy:");
-        } else if ((playerSelected) && (!enemySelected)){
-            enemeySelectedId = $(this).attr("id");
+        } else if ((playerSelected) && (!enemySelected)) {
+            enemeySelectedId = $(selectedChar).attr("id");
             selectedChar.remove();
             enemySelected = true;
             $('#enemy').append(selectedChar);
@@ -106,5 +151,11 @@ $(function () {
             $('#info').text("Remaining enemies:");
             $('#fightBtn').prop('disabled', false);
         }
-     });
+    });
+
+    $('#fightBtn').click(function () { 
+        let player = eval(playerSelectedId);
+        let enemy = eval(enemeySelectedId);
+        $('#results').text(fightAction(player,enemy));
+    });
 });
